36ae438dde79911103f4b30a1de45df34e5ad1c0
GOD <pratikshrestha362@gmail.com>
2025-02-18 19:22:15 +0545

    Working on add_matrix

--- main.py
+++ main.py
@@ -6,23 +6,36 @@ class Matrix:
         self.matrixA = None
         self.matrixB = None
         self.matrixC= None
+        self.ans= None
+        self.data = {1:self.matrixA, 2:self.matrixB, 3:self.matrixC}
 
-    def show_matrix(self):
+    def display_matlist(self)->int:
+        """This function displays the matrix list and return given matrix number"""
+        print('''
+        Matrix
+        [1]. Matrix A
+        [2]. Matrix B
+        [3]. Matrix C
+        [4]. Back
+        :: ''')
+
+    def show_matrix_data(self):
         while True:
             try:
                 user_ip = int(input('''
+                Show Matrix
                 [1]. Matrix A
                 [2]. Matrix B
                 [3]. Matrix C
                 [4]. Back
-                '''))
+                :: '''))
                 match user_ip:
                     case 1:
-                        print(f"Matrix A = {self.matrixA}")
+                        print(f"Matrix A = \n{self.matrixA}")
                     case 2:
-                        print(f"Matrix B = {self.matrixB}")
+                        print(f"Matrix B = \n{self.matrixB}")
                     case 3:
-                        print(f"Matrix C = {self.matrixC}")
+                        print(f"Matrix C = \n{self.matrixC}")
                     case 4:
                         return
                     case _:
@@ -35,6 +48,7 @@ class Matrix:
         while True:
             try:
                 user_ip = int(input(f'''
+                Input for 
                 [1]. Matrix A
                 [2]. Matrix B
                 [3]. Matrix C
@@ -51,31 +65,48 @@ class Matrix:
                         return
                     case _:
                         print("Invalid selection! Please choose a valid option.")
-                        continue
             except ValueError:
                 print("Please Enter valid input")
 
     def input_matrix(self, matrix_name:str):
         """ function to take input for matrices """
-        try:
-            size = tuple(map(int, input(f"Enter matrix {matrix_name} size in n,m format: ").split(',')))
-            matrix = np.zeros(size, dtype=int)
+        while True:
+            try:
+                size = tuple(map(int, input(f"Enter matrix {matrix_name} size in n,m format: ").split(',')))
+                # Validate size format (two positive integers)
+                if len(size) != 2 or size[0] <= 0 or size[1] <= 0:
+                    print("❌ Error: Please enter two positive integers separated by a comma (e.g., 2,3).")
+                    continue 
+                matrix = np.zeros(size, dtype=int)
 
-            for i in range(size[0]):
-                for j in range(size[1]):
-                    matrix[i, j] = int(input(f"Enter value for {matrix_name}[{i},{j}]: "))
+                for i in range(size[0]):
+                    for j in range(size[1]):
+                        matrix[i, j] = int(input(f"Enter value for {matrix_name}[{i},{j}]: "))
 
-            print(f"Matrix {matrix_name} stored successfully!\n")
-            return matrix
+                print(f"Matrix {matrix_name} stored successfully!\n")
+                return matrix
 
-        except ValueError:
-            print("Invalid input! Please enter numbers only.")
-            return None
+            except ValueError:
+                print("Invalid input! Please enter valid input.")
+                return None
+
+    def add_matrix(self):
+        """Function that adds matrices"""
+        while True:
+            try:
+                print("Enter matrix numbers that you want to add ")
+                self.display_matlist()
+                user_ip = tuple(map(int,input("Plese enter giving comma (e.g. 2,3): ").split(',')))
+                for i in user_ip:
+                    self.ans += self.data.get(i) 
+
+            except ValueError:
+                print('Invalid input! Please enter valid input.')
 
     def operations(self):
         while True:
             print(f'''
-            What operation do you want to perform?\n
+            What operation do you want to perform?
             [1]. Input
             [2]. Data
             [3]. Add
@@ -86,12 +117,14 @@ class Matrix:
             [8]. Eigenvalues and Eigenvectors
             [9]. Exit
             ''')
-            user_ip = int(input())
+            user_ip = int(input('::'))
             match user_ip:
                 case 1:
-                    self.show_matrix()
-                case 2:
                     self.take_input()
+                case 2:
+                    self.show_matrix_data()
+                case 3:
+                    self.add_matrix()
                 case 9:
                     break
 
